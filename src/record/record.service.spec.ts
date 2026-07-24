02b2f98f44ffc0b9143aae03feea46554c8ef1c0
mzxxzysy <jsyjj21@gmail.com>
2026-07-24 16:21:59 +0200

    🐛 Fix: 부글 기록 서비스 테스트 제거 (#44)

--- src/record/record.service.spec.ts
+++ src/record/record.service.spec.ts
@@ -1,8 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecordService } from './record.service';
 import { PrismaService } from '@/prisma/prisma.service';
-import { RecordErrorCode } from './record-error-code.enum';
-import { CreateRecordDto } from './dto/boogle-record.dto';
 
 describe('RecordService', () => {
   let service: RecordService;
@@ -31,67 +29,5 @@ describe('RecordService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-  });
-  it('create()가 데이터를 가공하여 성공적으로 반환한다', async () => {
-    const dto: Partial<CreateRecordDto> = {
-      regDate: '2026-07-11',
-      hasBowel: true,
-      stoolBristol: 4,
-      bowelFeeling: 'good',
-      stomach: 'good',
-    };
-
-    const mockDbRecord = {
-      id: 1,
-      userId: 1,
-      regDate: new Date('2026-07-11'),
-      hasBowel: false,
-      stoolBristol: 4,
-      stoolSimple: 'good',
-      bowelFeeling: 'good',
-      stomach: 'good',
-      distension: false,
-      remainingFeeling: false,
-      urgency: false,
-      takenTime: 5,
-      amount: 1,
-      color: 'brown',
-      status: 'NORMAL',
-      updateDate: new Date('2026-07-11'),
-    };
-
-    mockCreate.mockResolvedValue(mockDbRecord);
-
-    const result = await service.create(1, dto as CreateRecordDto);
-
-    expect(mockCreate).toHaveBeenCalledWith({
-      data: {
-        userId: 1,
-        hasBowel: true,
-        stoolBristol: 4,
-        bowelFeeling: 'good',
-        stomach: 'good',
-        stoolSimple: 'M',
-        regDate: new Date('2026-07-11'),
-      },
-    });
-
-    expect(result.regDate).toBe('2026-07-11');
-    expect(result.id).toBe(1);
-  });
-
-  it('배변한 경우 필수 정보가 누락되면 생성을 거부한다', async () => {
-    const dto: Partial<CreateRecordDto> = {
-      regDate: '2026-07-11',
-      hasBowel: true,
-    };
-
-    await expect(
-      service.create(1, dto as CreateRecordDto),
-    ).rejects.toMatchObject({
-      errorCode: RecordErrorCode.INVALID_BOWEL_REQUEST,
-      status: 400,
-    });
-    expect(mockCreate).not.toHaveBeenCalled();
   });
 });
