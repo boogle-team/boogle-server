4ce789eb82e6b7fb576da3fb960128aaf2e6bd7e
mzxxzysy <jsyjj21@gmail.com>
2026-07-24 16:13:40 +0200

    🐛 Fix: 부글 기록 jwt 인증 적용 (#44)

--- src/record/record.module.ts
+++ src/record/record.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RecordController } from './record.controller';
 import { RecordService } from './record.service';
+import { AuthModule } from '@/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [RecordController],
   providers: [RecordService],
 })
