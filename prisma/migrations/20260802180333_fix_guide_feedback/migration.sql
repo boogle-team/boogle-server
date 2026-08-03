46f3433ed56b4e27d4717e93a2a10ad04573d0b7
seongsoon1818 <ykdl0327@gmail.com>
2026-08-03 21:01:00 +0900

    🐛 Fix: 마이그레이션 파일 순서로 인한 오류 해결
    마이그레이션 파일 인덱스 생성 순서로 인해 발생하는 오류 해결

--- prisma/migrations/20260802180333_fix_guide_feedback/migration.sql
+++ prisma/migrations/20260802180333_fix_guide_feedback/migration.sql
@@ -5,12 +5,6 @@
   - Added the required column `week_start_date` to the `guide_feedback` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropForeignKey
-ALTER TABLE `guide_feedback` DROP FOREIGN KEY `guide_feedback_user_id_fkey`;
-
--- DropIndex
-DROP INDEX `guide_feedback_index_5` ON `guide_feedback`;
-
 -- AlterTable
 ALTER TABLE `guide_feedback` ADD COLUMN `week_start_date` DATE NOT NULL;
 
@@ -20,5 +14,5 @@ CREATE INDEX `guide_feedback_user_week_idx` ON `guide_feedback`(`user_id`, `week
 -- CreateIndex
 CREATE UNIQUE INDEX `guide_feedback_user_guide_week_uq` ON `guide_feedback`(`user_id`, `guide_id`, `week_start_date`);
 
--- AddForeignKey
-ALTER TABLE `boogle_record` ADD CONSTRAINT `boogle_record_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `member`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+-- DropIndex
+DROP INDEX `guide_feedback_index_5` ON `guide_feedback`;
